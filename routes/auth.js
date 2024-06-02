74bca613948c44229b264417318705e638d6af1f
Carlo Ramirez <carloramirez1295@gmail.com>
2024-06-01 21:55:59 -0500

    Fin Seccion 12-Google

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@
 
 */
 const { check } = require('express-validator')
-const { login } = require('../controllers/auth')
+const { login, googleSignIn } = require('../controllers/auth')
 const { Router } = require('express');
 const { validarcampos } = require('../middlewares/validar-campos');
 
@@ -11,12 +11,19 @@ const { validarcampos } = require('../middlewares/validar-campos');
 const router = Router();
 
 router.post('/',
-    [
-        check('email', 'El email es obligatorio').isEmail(),
-        check('password', 'El password es obligatorio').not().isEmpty(),
-        validarcampos
-    ],
-    login
+  [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    validarcampos
+  ],
+  login
+)
+router.post('/google',
+  [
+    check('token', 'El token de google es obligatorio').not().isEmpty(),
+    validarcampos
+  ],
+  googleSignIn
 )
 
 module.exports = router;
